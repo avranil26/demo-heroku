ce6a321ec39fedf2075f460590e32fc209083a29
Avranil Basu <34956714+avranil26@users.noreply.github.com>
2020-12-22 15:57:53 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+from sklearn import datasets
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.externals import joblib
+
+
+# In[23]:
+
+
+url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"
+df = pd.read_csv(url)
+include = ['Age', 'Sex', 'Embarked', 'Survived'] # Only four features
+df_ = df[include]
+
+# Data Preprocessing
+categoricals = []
+for col, col_type in df_.dtypes.iteritems():
+     if col_type == 'O':
+          categoricals.append(col)
+     else:
+          df_[col].fillna(0, inplace=True)
+
+df_ohe = pd.get_dummies(df_, columns=categoricals, dummy_na=True)
+
+# Logistic Regression classifier
+from sklearn.linear_model import LogisticRegression
+dependent_variable = 'Survived'
+x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+y = df_ohe[dependent_variable]
+lr = LogisticRegression()
+lr.fit(x, y)
+
+# Save your model
+from sklearn.externals import joblib
+joblib.dump(lr, 'model.pkl')
+print("Model dumped!")
+
+# Load the model that you just saved
+lr = joblib.load('model.pkl')
+
+# Saving the data columns from training
+model_columns = list(x.columns)
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
+
+
+# In[ ]:
+
+
+
+
